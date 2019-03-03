c5ef3218be6c9034022ab1e50d08082956c252fb
makometr <makometr@yandex.ru>
2019-03-03 21:26:47 +0300

    Size of windows changed

--- Lab_1/AppWin.cpp
+++ Lab_1/AppWin.cpp
@@ -3,7 +3,7 @@
 AppWindow::AppWindow(int W,int H,const char*L)
     : Fl_Window(W,H,L)
 {
-    glSubWin = new GlSubWin(10, 10, w()-20, h()-80);
+    glSubWin = new GlSubWin(10, 10, w()-200, h()-20);
 
     end();
 }
